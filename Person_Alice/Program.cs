ac43965d82d57e4942861d00437edc4df7b03c49
Megan <meganebsen@gmail.com>
2025-03-02 14:47:06 +0100

    tilføjet opgaver om dcd og oop

---
+++ Person_Alice/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Person_Alice
+{
+
+    class Student
+    {
+        private string name;
+        private int grade;
+
+        public void SetGrade(int g)
+        {
+            if (g >= 0 && g <= 100)
+                grade = g;
+            else
+                Console.WriteLine("Invalid grade");
+        }
+
+        public string Name
+        {
+            get { return name; } // Get method
+            set { name = value; } // Set method
+        }
+
+        public int Grade
+        {
+            get => grade;
+            set => grade = value;
+        }
+    }
+
+        internal class Program
+        {
+            static void Main(string[] arg)
+            {
+                Student s = new Student();
+                s.Name = "Alice";  // ❌ Fejl!
+                s.Grade = 90;      // ❌ Fejl!
+                s.SetGrade(85);    // ✅ Fungerer
+
+            Console.WriteLine(s.Name + s.Grade);
+
+                Console.ReadLine();
+            }
+        }
+    }
+
---
+++ Person_classes/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Person_classes
+{
+
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        
+        public void Speak()
+        {
+            Console.WriteLine("Hej jeg hedder " + Name + ". Jeg er " + Age + " år gammel"); 
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Megan";
+            person.Age = 34;
+            
+            person.Speak();
+
+            Console.ReadLine();
+
+        }
+    }
+}
